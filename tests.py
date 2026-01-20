4fbfb2cdc3b1fd548500e2940c8ad4f7887750e3
朗月 <zhaoyu.qin@foxmail.com>
2026-01-20 10:02:30 +0800

    feat: 完成高阶反向计算图的推理分析

--- tests.py
+++ tests.py
@@ -36,28 +36,28 @@ from dezero.utils import plot_dot_graph
 # ----------Rosenbrock函数的反向梯度优化
 
 
-def rosenbrock(x0, x1):
-    y = 100 * (x1 - x0**2) ** 2 + (x0 - 1) ** 2
-    return y
+# def rosenbrock(x0, x1):
+#     y = 100 * (x1 - x0**2) ** 2 + (x0 - 1) ** 2
+#     return y
 
 
-x0 = Variable(np.array(0.0))
-x1 = Variable(np.array(2.0))
-lr = 0.001
-iters = 1000
+# x0 = Variable(np.array(0.0))
+# x1 = Variable(np.array(2.0))
+# lr = 0.001
+# iters = 1000
 
-for i in range(iters):
-    y = rosenbrock(x0, x1)
+# for i in range(iters):
+#     y = rosenbrock(x0, x1)
 
-    x0.cleargrad()
-    x1.cleargrad()
-    y.backward()
+#     x0.cleargrad()
+#     x1.cleargrad()
+#     y.backward()
 
-    # 变量的梯度向量方向是其函数值增加最快的方向，所以逆梯度方向是函数值下降最快的方向。
-    # 梯度方向是增加最快方向可以通过方向导数来说明，grad_n = grad * n * cosθ
-    x0.data -= lr * x0.grad
-    x1.data -= lr * x1.grad
-print(x0, x1)
+#     # 变量的梯度向量方向是其函数值增加最快的方向，所以逆梯度方向是函数值下降最快的方向。
+#     # 梯度方向是增加最快方向可以通过方向导数来说明，grad_n = grad * n * cosθ
+#     x0.data -= lr * x0.grad
+#     x1.data -= lr * x1.grad
+# print(x0, x1)
 
 
 # -------------------- Sin函数的反向传播
@@ -82,12 +82,19 @@ else:
 x.cleargrad()  # x 重用，但 grad 归零
 y = F.sin(x)
 y.backward(create_graph=True)
+# 正向计算图
 plot_dot_graph(y, verbose=True, to_file="./results/sin_y.png")
 
 n = 3
 for i in range(n):
     gx = x.grad
     gx.name = "gx" + str(i + 1)
+    # 反向求导计算图，这里注意：
+    # 1. 计算图没有正、反之分，都是从一个输出端开始追溯计算过程，只是这个过程是正是反
+    # 2. 通过正向计算图反推反向计算图，但是一般反向过程更复杂，这是由于求导操作，
+    #    即 backward() 函数复杂度增加导致的，然而这依然是可以逐层逐步追溯的。
+    # 3. 注意每一层backward()函数的输入梯度。
+    # 4. 高阶导数反向计算图可视化过程可能会有剪枝情况，通常分析对原始x的导数。
     plot_dot_graph(gx, False, f"./results/sin_gx{i+1}.png")
 
     x.cleargrad()  # x.grad 归零
